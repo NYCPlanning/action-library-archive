7e8cb18152b1c178b70983cf17f4df73cf6b3f52
Sasha Weinstein <70385853+SashaWeinstein@users.noreply.github.com>
2022-09-13 14:35:22 -0400

    use old entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-
-echo "entrypoint.sh called"
-
 if [ "$1" != "" ] ; then name="--name $1"; else name=''; fi
 if [ "$2" != "" ] ; then path="--path $2"; else path=''; fi
 if [ "$4" = "true" ] ; then s3="--s3"; else s3=''; fi
@@ -11,10 +8,10 @@ if [ "$7" != "" ] ; then version="--version $7"; else version=''; fi
 
 for format in $3
 do
-  python3 -m library.cli archive $name $path -o $format $s3 $compress $latest $version &
+  library archive $name $path -o $format $s3 $compress $latest $version &
 done
 
-# wait
+wait
 
-# show=$(library show $1 --json)
-# echo "::set-output name=show::$show"
+show=$(library show $1 --json)
+echo "::set-output name=show::$show"
